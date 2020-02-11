1de92fc2ab7445c3d83c62adf50e9d453669ee35
SufjanHe <hez66@mac1xa3.ca>
2020-02-11 22:50:13 +0000

    Checkout Latest Merge

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -3,7 +3,13 @@
 menu(){
 	echo "Execute 'Checkout Latest Merge' by typing 1"
 	echo "Execute 'File Size List' by typing 2"
-	echo "Execute 'File Type Count' by typing 4"
+	echo "Execute 'File Type Count' by typing 3"
 	read -p "I wanna doooooo:" choice
 }
 menu
+
+if [ $choice -eq 1 ]; then
+	line=`git log --grep='Merge' -i --oneline|head -1`
+	commit=${line:0:7}
+	git checkout $commit
+fi
