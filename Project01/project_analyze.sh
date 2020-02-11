cdd69970ef2f9cfbc7b41aef6db8b1a015761518
SufjanHe <hez66@mac1xa3.ca>
2020-02-11 22:24:28 +0000

    Script Input

---
+++ Project01/project_analyze.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+menu(){
+	echo "Execute 'Checkout Latest Merge' by typing 1"
+	echo "Execute 'File Size List' by typing 2"
+	echo "Execute 'File Type Count' by typing 4"
+	read -p "I wanna doooooo:" choice
+}
+menu
