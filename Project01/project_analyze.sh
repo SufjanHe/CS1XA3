8157369fb1f7568354197e0772f1404eed5548e4
SufjanHe <hez66@mac1xa3.ca>
2020-02-11 23:16:56 +0000

    small improvement

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -12,10 +12,17 @@ if [ $choice -eq 1 ]; then
 	line=`git log --grep='Merge' -i --oneline|head -1`
 	commit=${line:0:7}
 	git checkout $commit
+	read -p "Want More?(yes/no)" more
 elif [ $choice -eq 2 ]; then
 	du -ah ..|sort -rh
+	read -p "Want More?(yes/no)" more
 elif [ $choice -eq 3 ]; then
 	read -p"Which extension would like to seeeeee?" extension
 	num=`find .. -name "*.$extension"|wc -l`
 	echo "There are "$num" files with the extention "$extension" in repo"
+	read -p "Want More?(yes/no)" more
+fi
+
+if [ $more = "yes" ]; then
+	./project_analyze.sh
 fi
